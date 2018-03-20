83f140e2d131183ff1e847537d1202fdeffaa8fd
Julien JEHL <julien.jehl@roboticia.com>
2018-03-20 22:47:20 +0100

    check if process is running

--- django/app1/views.py
+++ django/app1/views.py
@@ -17,8 +17,8 @@ from.forms import AlertForm
 def process():
     cam_path = os.path.join(settings.BASE_DIR,'app1/process_camera.py')
     alert_path = os.path.join(settings.BASE_DIR,'app1/process_alert.py')
-    return ([p for p in ps.process_iter() if cam_path in p.cmdline()],
-             [p for p in ps.process_iter() if alert_path in p.cmdline()])
+    return ([p for p in ps.process_iter() if cam_path in p.cmdline() and p.status()=='running'],
+             [p for p in ps.process_iter() if alert_path in p.cmdline() and p.status()=='running'])
 
 
 def index(request):

c175005f832536abef1ba8dd0cdee5944101d2d7
Julien JEHL <julien.jehl@roboticia.com>
2018-04-03 13:48:12 +0200

    path python2.7

--- django/app1/models.py
+++ django/app1/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from datetime import datetime
 
+@python_2_unicode_compatible
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
--- django/app1/urls.py
+++ django/app1/urls.py
@@ -1,22 +1,43 @@
+from django import VERSION as v
 from django.conf import settings
-from django.urls import path, include
 from django.conf.urls import url
 from django.conf.urls.static import static
-
 from . import views
 
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    url(r'^$', views.index, name='index'),
-    url(r'^camera/$', views.camera, name='camera'),
-    url(r'^darknet/$', views.darknet, name='darknet'),
-    url(r'^darknet/state/$', views.darknet_state, name='ds'),
-    path('panel/<int:first>', views.panel, name='panel'),
-    path('alert', views.alert, name='alertl'),
-    url(r'^panel/detail/(?P<id>\d+)$', views.panel_detail, name='detail'),
-    url(r'^settings/$', views.configuration, name='configuration'),
-    url(r'^settings/wifi_add/$', views.wifi_add, name='wifi_add'),
-    url(r'^settings/wifi_suppr/$', views.wifi_suppr, name='wifi_suppr'),
-    url(r'^settings/wifi_restart/$', views.wifi_restart, name='wifi_restart'),
-    url(r'^reboot/', views.reboot, name='reboot'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if v[0] >=2 : 
+    from django.urls import path, include
+    urlpatterns = [
+        path('accounts/', include('django.contrib.auth.urls')),
+        url(r'^$', views.index, name='index'),
+        url(r'^camera/$', views.camera, name='camera'),
+        url(r'^darknet/$', views.darknet, name='darknet'),
+        url(r'^darknet/state/$', views.darknet_state, name='ds'),
+        path('panel/<int:first>', views.panel, name='panel'),
+        path('alert', views.alert, name='alert'),
+        url(r'^panel/detail/(?P<id>\d+)$', views.panel_detail, name='detail'),
+        url(r'^settings/$', views.configuration, name='configuration'),
+        url(r'^settings/wifi_add/$', views.wifi_add, name='wifi_add'),
+        url(r'^settings/wifi_suppr/$', views.wifi_suppr, name='wifi_suppr'),
+        url(r'^settings/wifi_restart/$', views.wifi_restart, name='wifi_restart'),
+        url(r'^reboot/', views.reboot, name='reboot'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+else :
+    from django.conf.urls import include
+    urlpatterns = [
+        url(r'^accounts/$', include('django.contrib.auth.urls')),
+        url(r'^$', views.index, name='index'),
+        url(r'^camera/$', views.camera, name='camera'),
+        url(r'^darknet/$', views.darknet, name='darknet'),
+        url(r'^darknet/state/$', views.darknet_state, name='ds'),
+        url(r'^panel/(?P<first>\d+)$', views.panel, name='panel'),
+        url(r'^alert/$', views.alert, name='alert'),
+        url(r'^panel/detail/(?P<id>\d+)$', views.panel_detail, name='detail'),
+        url(r'^settings/$', views.configuration, name='configuration'),
+        url(r'^settings/wifi_add/$', views.wifi_add, name='wifi_add'),
+        url(r'^settings/wifi_suppr/$', views.wifi_suppr, name='wifi_suppr'),
+        url(r'^settings/wifi_restart/$', views.wifi_restart, name='wifi_restart'),
+        url(r'^reboot/', views.reboot, name='reboot'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    

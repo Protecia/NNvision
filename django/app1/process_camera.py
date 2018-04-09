244619565ef193be91d8ef8e35452ae48ea52815
jjehl <test@test.com>
2018-04-09 10:36:05 +0000

    py2

--- django/app1/process_camera.py
+++ django/app1/process_camera.py
@@ -72,7 +72,7 @@ class ProcessCamera(Thread):
         self.cam_id = cam.id-1
         self.nb_cam = nb_cam
         self.running = False
-        self.img_temp = os.path.join(settings.BASE_DIR,'tempimg_cam'+str(self.cam_id))
+        self.img_temp = os.path.join(settings.MEDIA_ROOT,'tempimg_cam'+str(self.cam_id))
         self.threshold = 0.4
         self.pos_sensivity = 100
         self.black_list=(b'pottedplant',b'cell phone')

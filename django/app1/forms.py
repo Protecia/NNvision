7bf92c7ce46164f99069d6b7e5c5f80a84271879
Julien JEHL <julien.jehl@roboticia.com>
2018-11-28 14:05:25 +0100

    auto

--- django/app1/forms.py
+++ django/app1/forms.py
@@ -20,7 +20,14 @@ class AlertForm(forms.ModelForm):
         
 DAY_CODE_STR= {'*':_('Every days'),
                '0':_('Monday'),
+               '1':_('Tuesday'),
+               '2':_('Wednesday'),
+               '3':_('Thursday'),
+               '4':_('Friday'),
+               '5':_('Saturday'),
+               '6':_('Sunday'),
                }
+
 
 DAY_CHOICES =   ((7,_('Every days')),
                  (0,_('Monday')),

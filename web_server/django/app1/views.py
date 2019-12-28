c6b7507b911d20971eeb525c467cdc8ad0b4fd48
Julien JEHL <julien.jehl@free.fr>
2019-12-28 11:58:45 +0100

    Update views.py

--- web_server/django/app1/views.py
+++ web_server/django/app1/views.py
@@ -9,17 +9,12 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Count
-from .models import Camera, Result, Alert, Client, Scheme, Object
+from .models import Camera, Result, Alert, Client
 from .forms import AlertForm, AutomatForm, DAY_CODE_STR
 from .process_alert import stop_adam_all
 from PIL import Image
 from django.utils import translation
 from crontab import CronTab
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.db import IntegrityError
-import json
-from django.db import transaction
 
 # Create your views here.
 
@@ -34,102 +29,6 @@ def process():
             pass
     return _process
 
-@csrf_exempt
-def setCam(request):
-    data = json.loads(request.body.decode())
-    try :
-        client = Client.objects.get(key=data['key'])
-    except :
-        time.sleep(10)
-        pass
-        return JsonResponse({'statut':False},safe=False)
-    cam = data['cam']
-    for c in cam:
-        cam, created = Camera.objects.update_or_create(client = client, ip = c['ip'], defaults=c)
-        try :
-            cam.save()
-        except IntegrityError:
-            pass
-    return JsonResponse({'statut':True},safe=False)
-
-
-@csrf_exempt
-def getScheme(request):
-    key = request.POST.get('key', 'default')
-    if len(Client.objects.filter(key=key))>0 :
-        scheme = Scheme.objects.all()
-        return JsonResponse(list(scheme.values()), safe=False)
-    return JsonResponse('0',safe=False)
-
-@csrf_exempt
-def uploadImage(request):
-    if request.method == 'POST':
-        key = request.POST.get('key', 'default')
-        file = request.FILES['myFile']
-        size = len(file)
-        return JsonResponse([{'size':size},],safe=False)
-    return "not post"
-
-@csrf_exempt
-def uploadResult(request):
-    data = json.loads(request.body.decode())
-    try :
-        client = Client.objects.get(key=data['key'])
-    except :
-        time.sleep(10)
-        pass
-        return JsonResponse({'statut':False},safe=False)
-    with transaction.atomic():
-       result_DB = Result(camera=data['cam'],img = client.id+data['img'], brut=data['result_darknet'])
-       for r in data['result_filtered']:
-           object_DB = Object(result = result_DB,
-                              result_object=r[0],
-                              result_prob=r[1],
-                              result_loc1=r[2][0],
-                              result_loc2=r[2][1],
-                              result_loc3=r[2][2],
-                              result_loc4=r[2][3])
-           object_DB.save()
-       result_DB.save()
-
-
-    return JsonResponse([{'statut':True},],safe=False)
-
-@csrf_exempt
-def getCam(request):
-    key = request.POST.get('key', 'default')
-    force = request.POST.get('force', '0')
-    cam = Camera.objects.filter(client__key=key)
-    if force=='1' or any([c.update for c in cam]):
-        return JsonResponse(list(cam.values()), safe=False)
-    time.sleep(20)
-    return JsonResponse(list(cam.values()), safe=False)
-
-@csrf_exempt
-def upCam(request):
-    key = request.POST.get('key', 'default')
-    Camera.objects.filter(active=True, client__key=key).update(update=False)
-    return HttpResponse('0')
-
-@csrf_exempt
-def getState(request):
-    key = request.POST.get('key', 'default')
-    i=0
-    while i<10 :
-        c = Client.objects.filter(key=key)
-        _c = c[0]
-        if not _c.change:
-            time.sleep(1)
-        else :
-            _c.change = False
-            _c.save()
-            break
-        i+=1
-    return JsonResponse(list(c.values()), safe=False)
-
-
-
-
 
 def index(request):
     user_language = 'fr'
@@ -140,6 +39,7 @@ def index(request):
         return redirect('/warning/0')
     if not request.user.is_authenticated or not request.user.has_perm('app1.camera'):
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    request.session['client']=Client.object.get(Profile__user=request.user)
     d_action = request.POST.get('d_action')
     if d_action == 'start' and len(process()[0])==0:
         Camera.objects.all().update(rec=True)
@@ -249,7 +149,7 @@ def panel(request, nav, first):
         else :
             request.session['class']= 'all'
     filter_class = request.session.get("class","all")
-    if not Result.objects.all():
+    if not Result.objects.filter(camera__client=request.session['client']):
         img_array = []
         img_alert_array = []
         form = AlertForm()
@@ -258,40 +158,40 @@ def panel(request, nav, first):
         return render(request, 'app1/panel.html', context)
     if filter_class == "all" :
         if nav == 'd':
-            imgs = Result.objects.all().order_by('-id')[first:first+12]
+            imgs = Result.objects.filter(camera__client=request.session['client']).order_by('-id')[first:first+12]
             if imgs :
                 time_result = imgs[0].time
             else:
-                time_result = Result.objects.all().earliest('time').time
-            imgs_alert = Result.objects.filter(alert=True, time__lte=time_result).order_by('-id')[:3]
-            first_alert = len(Result.objects.filter(alert=True, time__gt=time_result))
+                time_result = Result.objects.filter(camera__client=request.session['client']).earliest('time').time
+            imgs_alert = Result.objects.filter(alert=True,camera__client=request.session['client'], time__lte=time_result).order_by('-id')[:3]
+            first_alert = len(Result.objects.filter(alert=True, camera__client=request.session['client'], time__gt=time_result))
         if nav == 'a':
-            imgs_alert = Result.objects.filter(alert=True).order_by('-id')[first:first+3]
+            imgs_alert = Result.objects.filter(alert=True, camera__client=request.session['client']).order_by('-id')[first:first+3]
             if imgs_alert :
                 time_result = imgs_alert[0].time
             else :
-                time_result = Result.objects.filter(alert=True).earliest('time').time
-            imgs = Result.objects.filter(time__lte=time_result).order_by('-id')[:12]
+                time_result = Result.objects.filter(alert=True, camera__client=request.session['client']).earliest('time').time
+            imgs = Result.objects.filter(camera__client=request.session['client'], time__lte=time_result).order_by('-id')[:12]
             first_alert = first
-            first = len(Result.objects.filter(time__gt=time_result))
+            first = len(Result.objects.filter(camera__client=request.session['client'],time__gt=time_result))
     else :
         if nav == 'd':
-            imgs = Result.objects.filter(object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[first:first+12]
+            imgs = Result.objects.filter(camera__client=request.session['client'],object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[first:first+12]
             if imgs :
                 time_result = imgs[0].time
             else:
-                time_result = Result.objects.all().earliest('time').time
-            imgs_alert = Result.objects.filter(alert=True, time__lte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[:3]
-            first_alert = len(Result.objects.filter(alert=True, time__gte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object')))
+                time_result = Result.objects.filter(camera__client=request.session['client']).earliest('time').time
+            imgs_alert = Result.objects.filter(alert=True, camera__client=request.session['client'], time__lte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[:3]
+            first_alert = len(Result.objects.filter(alert=True, camera__client=request.session['client'], time__gte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object')))
         if nav == 'a':
-            imgs_alert = Result.objects.filter(alert=True, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[first:first+3]
+            imgs_alert = Result.objects.filter(alert=True, camera__client=request.session['client'], object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[first:first+3]
             if imgs_alert :
                 time_result = imgs_alert[0].time
             else :
-                time_result = Result.objects.filter(alert=True).earliest('time').time
-            imgs = Result.objects.filter(time__lte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[:12]
+                time_result = Result.objects.filter(alert=True, camera__client=request.session['client']).earliest('time').time
+            imgs = Result.objects.filter(camera__client=request.session['client'], time__lte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object'))[:12]
             first_alert = first
-            first = len(Result.objects.filter(time__gte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object')))
+            first = len(Result.objects.filter(camera__client=request.session['client'], time__gte=time_result, object__result_object__contains=filter_class).order_by('-id').annotate(c=Count('object__result_object')))
     img_array = [imgs[i:i + 3] for i in range(0, len(imgs), 3)]
     img_alert_array = [imgs_alert[i:i + 3] for i in range(0, len(imgs_alert), 3)]
     form = AlertForm()

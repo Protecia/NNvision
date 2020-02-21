fef4caf27714a5452d07003fc3c4fd015ba22226
Julien JEHL <julien.jehl@roboticia.com>
2020-02-21 13:24:16 +0100

    prod

--- python_client/settings/settings.py
+++ python_client/settings/settings.py
@@ -6,7 +6,7 @@ Created on Tue Dec  3 15:06:16 2019
 """
 import logging
 
-LOG = logging.WARNING
+LOG = logging.ERROR
 
 CFG = 'cfg/yolov3-spp.cfg'
 WEIGHTS = '../weights/yolov3-spp.weights'
@@ -18,5 +18,5 @@ THREATED_REQUESTS=True
 
 QUEUE_SIZE = 100 # number of images to queue at max
 
-KEY = '9c618a8e4ef4c95b910a3386940d63c4bc72df3c8b2d0de56bf1709d7312a1a0'
-SERVER = 'http://192.168.1.18:2222/'
+KEY = '###'
+SERVER = 'https://client.protecia.com/'

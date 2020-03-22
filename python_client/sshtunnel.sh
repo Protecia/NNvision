088161b0ef80fb031ed6fb75c6dcb20ad043f145
Julien JEHL <julien.jehl@roboticia.com>
2020-03-22 16:51:02 +0100

    ssh tunnel

--- python_client/sshtunnel.sh
+++ python_client/sshtunnel.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 port=`python3 port_ssh_tunnel.py`
-if pgrep -f "localhost:23" >/dev/null
-then
-    echo "ssh tunnel 1 is running"
-else
-    echo "ssh tunnel 1 stopped"
-    sshpass -p 'protecia2020' ssh -o StrictHostKeyChecking=no -N -R $port:localhost:23 -p 2222 sshtunnel@client.protecia.com &
-fi
-if pgrep -f "localhost:2525" >/dev/null
-then
-    echo "ssh tunnel 2 is running"
-else
-    echo "ssh tunnel 2 stopped"
-    sshpass -p 'protecia2020' ssh -o StrictHostKeyChecking=no -N -R $(($port+1)):localhost:2525 -p 2222 sshtunnel@client.protecia.com &
-fi
+sshpass -p 'protecia2020' autossh -o StrictHostKeyChecking=no -N -R $port:localhost:22 -p 2222 sshtunnel@client.protecia.com &
+sshpass -p 'protecia2020' autossh -o StrictHostKeyChecking=no -N -R $(($port+1)):localhost:2525 -p 2222 sshtunnel@client.protecia.com &

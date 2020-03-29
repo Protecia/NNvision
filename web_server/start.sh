71530757c58d954af1a6124270993742493fd6c7
Julien JEHL <julien.jehl@roboticia.com>
2020-03-29 20:42:27 +0200

    auto start

--- web_server/start.sh
+++ web_server/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+service postgresql start
+sleep 5
+service cron start
+service apache2 start
 python3 /NNvision/django/app1/telegramBot.py &
 python3 /NNvision/django/app1/check_client_connection.py &
-service cron start
-service postgresql start
-service apache2 start
 tail -F /var/log/apache2/error.log

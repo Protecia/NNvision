c49c2c084dd9fb960066ab2ccaf61339d05daae9
Julien JEHL <julien.jehl@roboticia.com>
2020-04-04 15:00:56 +0200

    timeout request

--- python_client/upload.py
+++ python_client/upload.py
@@ -22,9 +22,9 @@ def uploadImageRealTime(Q):
                    'result' : json.dumps([(r[0].decode(),r[1],r[2]) for r in result]), 'real_time' : True,
                    'resize_factor':resize_factor}
         try :
-            r = requests.post(settings.SERVER+"uploadimage", files=files, data = imgJson)
+            r = requests.post(settings.SERVER+"uploadimage", files=files, data = imgJson,  timeout= 40)
             logger.warning('send json image real : {}'.format(r.status_code))
-        except requests.exceptions.ConnectionError :
+        except (requests.exceptions.ConnectionError, requests.Timeout) :
             logger.warning('uploadImageRealTime Can not find the remote server')
             time.sleep(5)
             pass
@@ -40,11 +40,11 @@ def uploadImage(Q):
             imgJson = {'key': settings.KEY, 'img_name': img_name, 'cam' : cam,
                        'result' : json.dumps([(r[0].decode(),r[1],r[2]) for r in result]), 'real_time' : False}
         try :
-            r = requests.post(settings.SERVER+"uploadimage", files=files, data = imgJson)
+            r = requests.post(settings.SERVER+"uploadimage", files=files, data = imgJson,  timeout= 40)
             server = True
             logger.info('send json image : {}'.format(imgJson))
             logger.warning('send image to server  : {}'.format(r.status_code))
-        except requests.exceptions.ConnectionError :
+        except (requests.exceptions.ConnectionError, requests.Timeout) :
             server = False
             logger.warning('uploadImage Can not find the remote server')
             time.sleep(5)
@@ -60,11 +60,11 @@ def uploadResult(Q):
             img, cam,  result_filtered, result_darknet = result[0], result[1], [(r[0].decode(),r[1],r[2]) for r in result[2] ], [(r[0].decode(),r[1],r[2]) for r in result[3] ]
             resultJson = {'key': settings.KEY, 'img' : img, 'cam' : cam, 'result_filtered' : result_filtered, 'result_darknet' : result_darknet }
         try :
-            r = requests.post(settings.SERVER+"uploadresult", json=resultJson)
+            r = requests.post(settings.SERVER+"uploadresult", json=resultJson,  timeout= 40)
             server = True
             logger.info('send json : {}'.format(resultJson))
             logger.warning('send result to server  : {}'.format(r.text))
-        except requests.exceptions.ConnectionError :
+        except (requests.exceptions.ConnectionError, requests.Timeout) :
             server = False
             logger.warning('uploadResult Can not find the remote server')
             time.sleep(5)
@@ -74,7 +74,7 @@ def getState(E, camera_state):
     while True:
         try :
             logger.warning('getState before post')
-            r = requests.post(settings.SERVER+"getState", data = {'key': settings.KEY, } )
+            r = requests.post(settings.SERVER+"getState", data = {'key': settings.KEY, }, timeout= 40 )
             logger.warning('getState request post')
             data = json.loads(r.text)
             logger.warning('getState answer : {}'.format(data))
@@ -85,7 +85,7 @@ def getState(E, camera_state):
             on_camera = data['cam']
             for pk, state in on_camera.items():
                 [camera_state[int(pk)][index].set() if i else camera_state[int(pk)][index].clear() for index, i in enumerate(state)]
-        except requests.exceptions.ConnectionError :
+        except (requests.exceptions.ConnectionError, requests.Timeout) :
             logger.warning('getState Can not find the remote server')
             time.sleep(5)
             pass

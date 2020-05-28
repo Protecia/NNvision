4dcf3e613447e991ae10b3ad3d0147c50cc3c055
Julien JEHL <julien.jehl@roboticia.com>
2020-05-28 12:50:07 +0200

    add trainning

--- run_nnvision_server.sh
+++ run_nnvision_server.sh
@@ -1,1 +1,9 @@
-docker run  -p 2222:80 -v nn_data:/NNvision/media_root -v nn_settings:/NNvision/django/projet -v nn_db:/var/lib/postgresql/10/main -v nn_migrate1:/NNvision/django/app1/migrations -v nn_migrate2:/NNvision/django/app2/migrations -v nn_cron:/var/spool/cron/crontabs --rm nnvision_server:0.2 
+#!/bin/bash
+docker run  --name nnvision  -p 80:80 -p 443:443 \
+            -v /home/nnvision/training:/NNvision/training \
+            -v dev_data:/NNvision/media_root \
+            -v dev_settings:/NNvision/django/projet \
+            -v dev_db:/var/lib/postgresql/10/main \
+            -v dev_migrate1:/NNvision/django/app1/migrations \
+            -v dev_migrate2:/NNvision/django/app2/migrations \
+            -v dev_cron:/var/spool/cron/crontabs --rm ssl:latest

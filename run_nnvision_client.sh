47630d807d18e37df528d6641960bf89698904b9
unknown <julien.jehl@roboticia.com>
2020-10-20 11:19:02 +0200

    integrate

--- run_nnvision_client.sh
+++ run_nnvision_client.sh
@@ -1,2 +1,7 @@
-docker run --name nnvision  --net=host -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video -v nn_settings_client:/NNvision/settings -v nn_camera:/NNvision/camera --runtime=nvidia --rm roboticia/protecia:0.4 &
-
+docker run      --gpus=all --name nnvision --net=host \
+                -e NVIDIA_VISIBLE_DEVICES=all \
+                -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video  \
+                -v nn_settings_client:/NNvision/settings \
+                -v nn_camera:/NNvision/camera \
+                -v nn_ssh:/root/.ssh/id_rsa \
+                --rm roboticia/protecia:1.0 &
--- web_server/start.sh
+++ web_server/start.sh
@@ -5,4 +5,4 @@ service cron start
 service apache2 start
 python3 /NNvision/django/app1/telegramBot.py &
 python3 /NNvision/django/app1/check_client_connection.py &
-tail -F /var/log/apache2/error.log
+cd django && /NNvision/django/daphne.sh

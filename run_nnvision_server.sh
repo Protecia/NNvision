30c6faf3d358164569fd3cf876336ad108ff2614
Julien JEHL <Julien.jehl@jouvencia.net>
2021-03-03 09:47:54 +0100

    Update run_nnvision_server.sh

--- run_nnvision_server.sh
+++ run_nnvision_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker run  --name nnvision  -p 80:80 -p 443:443 \
-            -v /home/nnvision/training:/NNvision/training \
+docker run -it --name dev -p 8080:8080 -p 80:80 -p 443:443 \
             -v dev_data:/NNvision/media_root \
             -v dev_settings:/NNvision/django/projet \
-            -v dev_db:/var/lib/postgresql/10/main \
-            -v dev_migrate1:/NNvision/django/app1/migrations \
-            -v dev_migrate2:/NNvision/django/app2/migrations \
-            -v dev_cron:/var/spool/cron/crontabs --rm ssl:latest
+            -v dev_db_12:/var/lib/postgresql/12/main \
+            -v dev_mig1:/NNvision/django/app1/migrations \
+            -v dev_mig2:/NNvision/django/app2/migrations \
+            -v dev_mig3:/NNvision/django/app3/migrations \
+            -v dev_cron:/var/spool/cron/crontabs --rm dev:latest bash

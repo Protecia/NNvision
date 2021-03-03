f02d37dab6642e27e38716ba44bc1d618978ef06
Julien JEHL <Julien.jehl@jouvencia.net>
2021-03-03 09:37:21 +0100

    Update run_nnvision_client.sh

--- run_nnvision_client.sh
+++ run_nnvision_client.sh
@@ -1,2 +1,7 @@
-docker run --name nnvision  --net=host -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video -v nn_settings_client:/NNvision/settings -v nn_camera:/NNvision/camera --runtime=nvidia --rm roboticia/protecia:0.4 &
+docker run --name nnvision  --net=host --gpus=all \
+           -e NVIDIA_VISIBLE_DEVICES=all \
+		   -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video \
+		   -v nn_settings_client:/NNvision/settings \
+		   -v nn_cron:/var/spool/cron/crontabs \
+		   -v nn_camera:/NNvision/camera --rm jouvencia/nnvision_client:nano_tx1
 

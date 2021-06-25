e31cacf63883cf16e010e6bb37829ab0bcb3272f
Julien <julien.jehl@jouvencia.net>
2021-06-25 19:50:43 +0200

    cron from container

--- run_nnvision_client.sh
+++ run_nnvision_client.sh
@@ -1,7 +1,9 @@
-docker run --name nnvision  --net=host --gpus=all \
-           -e NVIDIA_VISIBLE_DEVICES=all \
-		   -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video \
-		   -v nn_settings_client:/NNvision/settings \
-		   -v nn_cron:/var/spool/cron/crontabs \
-		   -v nn_camera:/NNvision/camera --rm jouvencia/nnvision_client:nano_tx1
-
+docker run      -it --gpus=all --name nnvision --net=host \
+                -e NVIDIA_VISIBLE_DEVICES=all \
+                -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video  \
+                -v nn_settings_client:/NNvision/python_client/settings \
+                -v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api \
+                -v nn_camera:/NNvision/python_client/camera \
+                -v nn_ssh:/root/.ssh/id_rsa \
+                -v /proc/device-tree/chosen:/NNvision/uuid \
+                --rm roboticia/nnvision_jetson_nano:0.1 bash

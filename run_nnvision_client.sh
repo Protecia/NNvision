4ba7182cce60cc745652e289e4c838f40e99e627
Julien <julien.jehl@jouvencia.net>
2021-06-27 18:56:36 +0200

    link to host

--- run_nnvision_client.sh
+++ run_nnvision_client.sh
@@ -3,7 +3,7 @@ docker run      -it --gpus=all --name nnvision --net=host \
                 -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video  \
                 -v nn_settings_client:/NNvision/python_client/settings \
                 -v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api \
-                -v nn_camera:/NNvision/python_client/camera \
+                -v /home/nnvision/conf:/NNvision/python_client/camera \
                 -v nn_ssh:/root/.ssh/id_rsa \
                 -v /proc/device-tree/chosen:/NNvision/uuid \
                 --rm roboticia/nnvision_jetson_nano:0.1 bash

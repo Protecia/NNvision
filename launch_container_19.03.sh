e7cfcd36732a6292835b8e4993fa42f8f1ed3565
Julien <julien.jehl@jouvencia.net>
2021-09-30 20:12:25 +0200

    clean NNVISION

--- launch_container_19.03.sh
+++
@@ -1,3 +0,0 @@
-docker run --name nnvision --gpus all -p 80:80 -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video -v nnvision_data:/NNvision/media_root -v nnvision_settings:/NNvision/django/projet -v nnvision_db:/var/lib/postgresql/10/main -v nnvision_migrate1:/NNvision/django/app1/migrations -v nnvision_migrate2:/NNvision/django/app2/migrations -v nnvision_cron:/var/spool/cron/crontabs -v nnvision_time:/etc/nnvision_time -v ddclient:/etc/ddclient --rm nnvision:2.0 &
-
-
--- web_server/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-service postgresql start
-sleep 5
-service cron start
-service apache2 start
-python3 /NNvision/django/app1/telegramBot.py &
-python3 /NNvision/django/app1/check_client_connection.py &
-tail -F /var/log/apache2/error.log
